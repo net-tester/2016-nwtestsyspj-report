79e0f0e55478ddcec920b0fa3e26320a826c7f03
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2017-03-01 18:50:20 +0900

    Fix redpen issues.

--- run_redpen.sh
+++ run_redpen.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 redpen=/usr/local/redpen/bin/redpen
-redpen_conf=./redpen.xml
-redpen_log=redpen.log
-pandoc=/usr/bin/pandoc
+redpen_conf=redpen.xml
 
-rm -f $redpen_log
-for source in `ls *.tex | egrep -ve "(cites|main)"`
-do
-    $redpen -c $redpen_conf -f latex $source \
-               >> $redpen_log # drop url and table lines
-done
+function check_text () {
+    echo "# Check $1"
+    $redpen -c $redpen_conf -f latex -r plain2 $1 2>/dev/null
+}
+
+if test $1 -a -f $1
+then
+    check_text $1
+else
+    for file in `ls *.tex | egrep -ve "(cites|main)"`
+    do
+        check_text $file
+    done
+fi
+
